43cf990cb599670ecaef6fadf21c6b4528e6c156
Tyler W. Davis <tyler.davis@imperial.ac.uk>
2015-03-17 12:14:09 +0000

    general updates and improvements

--- tools/analysis/brazil_flux.py
+++ tools/analysis/brazil_flux.py
@@ -1,28 +1,88 @@
+#
+# brazil_flux.py
+#
+# written by Tyler W. Davis
+# Imperial College London
+#
+# 2015-03-16 -- created
+# 2015-03-17 -- last updated
+#
+# ~~~~~~~~~~~~
+# description:
+# ~~~~~~~~~~~~
+# This script reads the LBA Brazilflux data (downloaded from ORNL website) to
+# check data pairs (i.e., PPFD + NEE) within the nine sites.
+#
+###############################################################################
+## IMPORT MODULES:
+###############################################################################
 import datetime
 import glob
 import numpy
 import matplotlib.pyplot as plt
 
-#
-# CHECK BRAZIL SITES
-#
-my_dir = '/home/user/Desktop/CD32_BRAZIL_FLUX_NETWORK_1174/data/MAN_K34_QAQC/'
-my_files = glob.glob(my_dir + '*Avg_hour*csv')
-my_data = numpy.loadtxt(
-    fname=my_files[0],
-    dtype={'names': ('y', 'n', 'hh', 'par', 'Fc', 'fcraw', 'NEE'),
-           'formats' : ('i4', 'i4', 'i4', 'f4', 'f4', 'f4', 'f4')},
-    delimiter=',',
-    skiprows=6,
-    usecols=(0, 1, 2, 12, 22, 23, 26)
-)
+###############################################################################
+## FUNCTIONS:
+###############################################################################
+def find_files(d):
+    """
+    Name:     find_files
+    Input:    str, parent directory
+    Output:   dict
+    Features: Returns all the files from subdirs for Brazilflux
+    """
+    my_subdirs = ['MAN_K34', 'RON_FNS', 'SP_PDG', 
+                  'STM_K77', 'TOC_BAN', 'PA_CAX', 
+                  'RON_RJA', 'STM_K67', 'STM_K83', 
+                  'MAN_K34_QAQC', 'RON_FNS_QAQC', 'SP_PDG_QAQC', 
+                  'STM_K77_QAQC', 'TOC_BAN_QAQC', 'PA_CAX_QAQC', 
+                  'RON_RJA_QAQC', 'STM_K67_QAQC', 'STM_K83_QAQC']
+    my_search = '*Avg_hour*csv'
+    my_return = {}
+    #
+    for sd in my_subdirs:
+        my_pattern = d + sd + '/' + my_search
+        my_files = glob.glob(my_pattern)
+        if my_files:
+            my_return[sd] = my_files[0]
+    #
+    return my_return
 
-p0 = my_data.shape[0]
-p1 = numpy.where((my_data['par'] != -9999.) & (my_data['Fc'] != -9999.))
-p2 = numpy.where((my_data['par'] != -9999.) & (my_data['fcraw'] != -9999.))
+###############################################################################
+## MAIN:
+###############################################################################
+mac = True
+if mac:
+    my_dir = '/Users/twdavis/Projects/data/flux/brazil_flux/data/'
+else:
+    my_dir = '/home/user/Desktop/CD32_BRAZIL_FLUX_NETWORK_1174/data/'
+my_files = find_files(my_dir)
+my_pairs = {}
+for name in numpy.sort(my_files.keys()):
+    my_file = my_files[name]
+    my_data = numpy.loadtxt(
+        fname=my_file,
+        dtype={'names': ('y', 'n', 'hh', 'par', 'Fc', 'fcraw', 'NEE'),
+               'formats' : ('i4', 'i4', 'i4', 'f4', 'f4', 'f4', 'f4')},
+        delimiter=',',
+        skiprows=6,
+        usecols=(0, 1, 2, 12, 22, 23, 26)
+    )
+    #
+    p0 = my_data.shape[0]
+    p1 = numpy.where((my_data['par'] != -9999.) & (my_data['Fc'] != -9999.))
+    p2 = numpy.where((my_data['par'] != -9999.) & (my_data['fcraw'] != -9999.))
+    p3 = numpy.where((my_data['par'] != -9999.) & (my_data['NEE'] != -9999.))
+    #
+    my_pairs[name] = (p0, p1[0].shape[0], p2[0].shape[0], p3[0].shape[0])
 
-print "PAR/Fc    :", len(p1[0]), "/", p0
-print "PAR/fcraw :", len(p2[0]), "/", p0
+print "Station,PAR:Fc,PAR:Fcraw,PAR:NEE,TOTAL"
+for pair in numpy.sort(my_pairs.keys()):
+    print "%s,%d,%d,%d,%d" %(pair, 
+                             my_pairs[pair][1], 
+                             my_pairs[pair][2],
+                             my_pairs[pair][3],
+                             my_pairs[pair][0])
 
 my_ts = [datetime.datetime(my_data['y'][i], 1, 1) for i in xrange(p0)]
 for i in xrange(p0):

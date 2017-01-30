d86741ff692d113adf8c09dce8304215bc25a737
twdavis <twd34@cornell.edu>
2017-01-29 22:41:43 -0500

    updated daily plotting

--- tools/plotting/plot_daily_gpp.py
+++ tools/plotting/plot_daily_gpp.py
@@ -22,64 +22,35 @@ import matplotlib.pyplot as plt
 ###############################################################################
 # FUNCTIONS
 ###############################################################################
-def get_elv(my_path, st_name):
+def get_meta(my_path, st_name):
     """
-    Name:     get_elv
+    Name:     get_meta
     Input:    - str, path metadata file (my_path)
               - str, station name (st_name)
     Output:   float, elevation, m (my_elv)
-    Features: Returns the station elevation from GePiSaT database metadata file
+    Features: Returns the station meta data acquired from the GePiSaT
+              database metadata file
     """
     try:
         my_data = numpy.loadtxt(fname=my_path,
-                                dtype={'names': ('stationid', 'ele'),
-                                       'formats': ('S6', 'f4')},
+                                dtype={'names': ('stationid', 'ele', 'classid',
+                                                 'climateid'),
+                                       'formats': ('S6', 'f4', 'S3', 'S3')},
                                 delimiter=',',
                                 skiprows=1,
-                                usecols=(4, 8),
+                                usecols=(4, 8, 9, 11),
                                 converters={4: numpy.str,
-                                            8: numpy.float})
+                                            8: numpy.float,
+                                            9: numpy.str,
+                                            11: numpy.str})
     except:
-        return numpy.nan
+        return numpy.array([])
     else:
         my_idx = numpy.where(my_data['stationid'] == st_name)[0]
         if my_idx:
-            my_elv = my_data['ele'][my_idx[0]]
-            if my_elv == -9999:
-                my_elv = numpy.nan
+            return my_data[my_idx]
         else:
-            my_elv = numpy.nan
-
-        return my_elv
-
-
-def get_veg_type(my_path, st_name):
-    """
-    Name:     get_veg_type
-    Inputs:   - str, path meta data file (my_path)
-              - str, station name (st_name)
-    Output:   str, vegation type, short name
-    Features: Returns the vegation type for a given station
-    """
-    try:
-        my_data = numpy.loadtxt(fname=my_path,
-                                dtype={'names': ('stationid', 'classid'),
-                                       'formats': ('S6', 'S3')},
-                                delimiter=',',
-                                skiprows=1,
-                                usecols=(4, 9),
-                                converters={4: numpy.str,
-                                            9: numpy.str})
-    except:
-        return ''
-    else:
-        my_idx = numpy.where(my_data['stationid'] == st_name)
-        try:
-            my_veg = my_data['classid'][my_idx][0]
-        except:
-            my_veg = ''
-
-        return my_veg
+            return numpy.array([])
 
 
 def get_station_name(my_path):
@@ -122,10 +93,10 @@ def load_daily_gpp(my_path):
     return my_data
 
 
-def plot_daily_gpp(daily_data):
+def plot_daily_gpp(daily_data, my_text):
     """
     """
-    # props = dict(boxstyle='round', facecolor='wheat', alpha=0.5)
+    props = dict(boxstyle='round', facecolor='wheat', alpha=0.5)
     # plot_max = numpy.concatenate((my_fit, my_obs)).max()
     fig = plt.figure()
     ax1 = fig.add_subplot(111)
@@ -146,8 +117,8 @@ def plot_daily_gpp(daily_data):
     ax1.set_xlabel('Date', fontsize=12)
     # ax1.legend(bbox_to_anchor=(0., 1.02, 1., .102), loc=3,
     #            ncol=3, mode="expand", borderaxespad=0., fontsize=14)
-    # ax1.text(0.05, 0.95, my_text, transform=ax1.transAxes, fontsize=14,
-    #        verticalalignment='top', bbox=props)
+    ax1.text(0.05, 0.95, my_text, transform=ax1.transAxes, fontsize=12,
+             verticalalignment='top', bbox=props)
     plt.show()
 
 
@@ -178,13 +149,23 @@ def plot_monthly_gpp(daily_data):
 # MAIN PROGRAM
 ###############################################################################
 if __name__ == "__main__":
-    my_file = "AT-Neu_daily_GPP.txt"
+    my_file = "FI-Sod_daily_GPP.txt"
     flx_met_path = "Fluxdata_Met-Data.csv"
     my_data = load_daily_gpp(my_file)
     my_station = get_station_name(my_file)
-    my_veg_type = get_veg_type(flx_met_path, my_station)
-    my_elv = get_elv(flx_met_path, my_station)
-    my_txt1 = ("$\\mathrm{%s}$ ($\\mathrm{%s}$)\n$z=%0.1f$ m") % (
-        my_station, my_veg_type, my_elv)
-    print "%s" % (my_txt1)
-    # plot_daily_gpp(my_data)
+    my_meta = get_meta(flx_met_path, my_station)
+    if my_meta.size == 1:
+        my_elv = my_meta['ele']
+        my_veg = my_meta['classid']
+        my_clim = my_meta['climateid']
+        if my_elv == -9999:
+            my_txt = ("$\\mathrm{%s}$ ($\\mathrm{%s}/\\mathrm{%s}$)") % (
+                my_station, my_veg, my_clim)
+        else:
+            my_txt = (
+                "$\\mathrm{%s}$ ($\\mathrm{%s}/\\mathrm{%s}$)\n"
+                "$z=%0.1f$ m") % (my_station, my_veg, my_clim, my_elv)
+        print("%s" % (my_txt))
+    else:
+        my_txt = ""
+    plot_daily_gpp(my_data, my_txt)
